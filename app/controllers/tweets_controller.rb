ce10071d42603fb23c7bce2d85b2421542f755ba
yoshitaka0729175 <67721834+yoshitaka0729175@users.noreply.github.com>
2020-07-31 09:48:49 +0900

    ユーザー登録実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
   def index
+    #binding.pry
     @tweets = Tweet.all
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_031252) do
+ActiveRecord::Schema.define(version: 2020_07_30_112926) do
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
